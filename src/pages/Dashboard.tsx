3f9266029de4744d75c77a68311a9ee8c063ef7a
joshuaas5 <joshuaas500@gmail.com>
2025-05-23 17:38:25 -0300

    Corrigir problemas: remover NotificationSummary do Dashboard e adicionar script para colunas de preferências

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -693,11 +693,8 @@ const Dashboard: React.FC = () => {
 
       <div className="px-4 mb-6">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-6">
-          <div className="lg:col-span-2">
+          <div className="lg:col-span-3">
             <SpendingChart transactions={transactions} days={30} />
-          </div>
-          <div className="lg:col-span-1">
-            <NotificationSummary maxItems={3} />
           </div>
         </div>
       </div>
